0b18cbd8895ead2d55d566fa8cbab89072a295bf
JuanMOLopes <juan.oliiveira2007@gmail.com>
2025-04-17 11:24:33 -0300

    Fix Nav.jsx

--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -4,21 +4,21 @@ function Navbar() {
   return (
     <nav>
       <div className="pizza-item">
-        <a href="/#Detalhe1" className="pizza-link pizza1">
+        <a href="/Detalhe 1" className="pizza-link pizza1">
           Pizza de Margarita
         </a>
         <h3>Pizza Margarita</h3>
         <p>Uma deliciosa pizza com molho de tomate, queijo e manjericão fresco.</p>
       </div>
       <div className="pizza-item">
-        <a href="#Detalhe2" className="pizza-link pizza2">
+        <a href="/Detalhe 2" className="pizza-link pizza2">
           Pizza de Pepperoni
         </a>
         <h3>Pizza Pepperoni</h3>
         <p>Clássica pizza com fatias de pepperoni e queijo derretido.</p>
       </div>
       <div className="pizza-item">
-        <a href="#Detalhe3" className="pizza-link pizza3">
+        <a href="/Detalhe 3" className="pizza-link pizza3">
           Pizza de Quatro Queijos
         </a>
         <h3>Pizza Quatro Queijos</h3>
