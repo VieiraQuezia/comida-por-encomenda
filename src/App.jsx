80633dd71d5fb7a988e15a5c076dde9073a0a5f4
MARIN-Zenin <marin.lucas009@gmail.com>
2025-04-17 08:26:04 -0300

    Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Detalhe1 from "./pages/Detalhe1";
+import Detalhe2 from "./pages/Detalhe2";
+import Detalhe3 from "./pages/Detalhe3";
+import Contato from "./pages/Contato";
+import Sobre from "./pages/Sobre";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Detalhe 1" element={<Detalhe1 />} />
+        <Route path="/Detalhe 2" element={<Detalhe2 />} />
+        <Route path="/Detalhe 3" element={<Detalhe3 />} />
+        <Route path="/Contato" element={<Contato />} />
+        <Route path="/Sobre Nós" element={<Sobre />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NavBar/Nav.jsx
@@ -0,0 +1,26 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav>
+          <a href="/#Detalhe1">
+            Destinos
+          </a>
+          <a href="#Detalhe2">
+            Sobre Nós
+          </a>
+          <a href="#Detalhe3">
+            Contato
+          </a>
+          <a href="#Contato">
+            Contato
+          </a>
+          <a href="#Sobre">
+            Contato
+          </a>
+
+    </nav>
+  );
+}
+
+export default Navbar;
